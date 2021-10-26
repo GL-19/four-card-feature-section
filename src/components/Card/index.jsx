dd16e9c02c9f1ed42cf7e36356b96ccb5ea6e911
GL-19 <loiolagl19@gmail.com>
2021-10-26 20:38:56 -0300

    Card estilizado

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -3,7 +3,7 @@ import { CardWrapper } from "./styles.js";
 export function Card({ title, text, img, borderColor, gridArea }) {
 	return (
 		<CardWrapper borderColor={borderColor} area={gridArea}>
-			<h1>{title}</h1>
+			<h2>{title}</h2>
 			<p>{text}</p>
 			{img && <img src={img} alt="" />}
 		</CardWrapper>
--- src/components/Card/styles.js
+++ src/components/Card/styles.js
@@ -1,6 +1,37 @@
 import styled from "styled-components";
 
 export const CardWrapper = styled.div`
-	border-top: 2px solid ${(props) => props.borderColor || "#ffffff"};
+	width: 400px;
+	height: 250px;
+	margin: 20px;
+	padding: 35px;
+
 	grid-area: ${(props) => props.area};
+
+	display: flex;
+	flex-direction: column;
+
+	background: #ffffff;
+	border-radius: 7px;
+	border-top: 4px solid ${(props) => props.borderColor || "#ffffff"};
+
+	font-family: "Poppins", sans-serif;
+	font-size: 15px;
+
+	h2 {
+		color: hsl(234, 12%, 34%);
+	}
+
+	p {
+		color: hsl(229, 6%, 66%);
+		font-weight: 200;
+	}
+
+	img {
+		width: 75px;
+		height: 75px;
+		margin-top: 30px;
+
+		align-self: flex-end;
+	}
 `;
